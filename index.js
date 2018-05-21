191c6fdab29d93a466376e58814805dcc228df2f
Anton Pupkov <pupant@gmail.com>
2018-05-20 20:57:05 -0700

    Minor refactor
    wow, this early on?

--- index.js
+++ index.js
@@ -7,18 +7,27 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (!message.content.startsWith(process.env.PREFIX) || !message.guild) return;
-    const command = message.content.split(' ')[0].substr(process.env.PREFIX.length);
-    const args = message.content.split(' ').slice(1).join(' ');
-    if (command === 'guide') return message.channel.send('https://git.io/d.js-heroku');
-    else if (command === 'invite') return message.channel.send(process.env.INVITE);
+    if (!message.content.startsWith(process.env.PREFIX) || !message.guild)
+    {
+        return;
+    }
     
-    if (message.content === 'ping') {
-        // send back "Pong." to the channel the message was sent in
+    const command = message.content.substr(process.env.PREFIX.length).split(' ')[0];
+    const args = message.content.split(' ').slice(1).join(' ');
+    if (command === 'guide')
+    {
+        return message.channel.send('https://git.io/d.js-heroku');
+    }
+    else if (command === 'invite')
+    {
+        return message.channel.send(process.env.INVITE);
+    }
+    else if (message.content === 'ping') {
         message.channel.send('Pong.');
     }
-    else if (message.content === 'hello') {
-        message.channel.send('Hi!');
+    else
+    {
+        message.channel.send('Chosen topic was ' + command);
     }
 });
 
